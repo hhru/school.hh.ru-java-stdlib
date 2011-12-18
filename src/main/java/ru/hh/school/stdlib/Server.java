17d20e7eca092eb40c65e72052677b8da885c81c
Andrew <sumin.andrew@gmail.com>
2011-12-19 01:54:59 +0400

    Nothing to see here
    Move along!

--- src/main/java/ru/hh/school/stdlib/Server.java
+++ src/main/java/ru/hh/school/stdlib/Server.java
@@ -4,15 +4,15 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class Server {
-  public Server(InetSocketAddress addr) {
-    throw new UnsupportedOperationException();
-  }
+	public Server(InetSocketAddress addr) {
+		throw new UnsupportedOperationException();
+	}
 
-  public void run() throws IOException {
-    throw new UnsupportedOperationException();
-  }
+	public void run() throws IOException {
+		throw new UnsupportedOperationException();
+	}
 
-  public int getPort() {
-    throw new UnsupportedOperationException();
-  }
+	public int getPort() {
+		throw new UnsupportedOperationException();
+	}
 }
--- src/main/java/ru/hh/school/stdlib/Substitutor3000.java
+++ src/main/java/ru/hh/school/stdlib/Substitutor3000.java
@@ -1,11 +1,11 @@
 package ru.hh.school.stdlib;
 
 public class Substitutor3000 {
-  public void put(String key, String value) {
-    throw new UnsupportedOperationException();
-  }
+	public void put(String key, String value) {
+		throw new UnsupportedOperationException();
+	}
 
-  public String get(String key) {
-    throw new UnsupportedOperationException();
-  }
+	public String get(String key) {
+		throw new UnsupportedOperationException();
+	}
 }
